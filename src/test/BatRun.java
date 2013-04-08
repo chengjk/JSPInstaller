7039c168dcf94cf304167ae4799c62a81f7783f0
chengjk_pc <chengjinkai@gmail.com>
2013-04-08 19:52:45 +0800

    edit javaTeat

--- src/test/BatRun.java
+++ src/test/BatRun.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 public class BatRun {
 	
 	public  static void main(String[] args){
-		
+		//String test="cmd /c start cmd.exe";
 		//String oracle="cmd /c start sqlplus sys/orcl as sysdba @e:/2.sql";
-		String mysql="cmd /c start mysql -u root <e:/3.sql";
+		String mysql="cmd /c mysql -u root < e:/3.sql";
 		Runtime run=Runtime.getRuntime();
 		try {
 			run.exec(mysql);
